a4879bb5e6d6d4de9cb12b3b4625bbb53faf1f2e
bonche <chebon85@gmail.com>
2021-04-01 17:55:37 +0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Text, SafeAreaView, StatusBar, FlatList, View, TouchableOpacity } from 'react-native';
+import TodoInput from "./TodoInput";
+import TodoItem from "./TodoItem";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+    const [todoItems, setTodoItems] = useState([{text: "Buy groceries", completed: true}, {text: "Make recipe video", completed: false}]);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+    function addTodoItem(_text) {
+        setTodoItems([...todoItems, {text:_text, completed: false}]);
+    }
+
+
+    function deleteTodoItem(_index){
+        let tempArr = [...todoItems];
+        tempArr.splice(_index, 1);
+        setTodoItems(tempArr)
+    }
+
+
+    function completeTodoItem(_index){
+        let tempArr = [...todoItems];
+        tempArr[_index].completed = true;
+        setTodoItems(tempArr)
+    }
+
+
+    return (
+        <>
+            <StatusBar barStyle={"light-content"} backgroundColor={"#212121"}/>
+            <SafeAreaView style={{padding: 16, justifyContent: 'space-between', flex: 1}}>
+                <Text style={{fontSize: 36, fontWeight: 'bold'}}>Todo</Text>
+                <FlatList
+                    data={todoItems}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({item, index}) => {
+                        return (
+                            <TodoItem
+                                item={item}
+                                deleteFunction={() => deleteTodoItem(index)}
+                                completeFunction={() => completeTodoItem(index)}
+                            />
+                        )
+                    }}
+                />
+                <TodoInput onPress={addTodoItem} />
+            </SafeAreaView>
+        </>
+    );
+};
+
+export default App;
---
+++ TodoItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Text, TouchableOpacity} from 'react-native';
+
+export default function TodoItem(props) {
+
+    // Update style according to props
+    let style = props.item.completed ? {
+        textDecorationLine: 'line-through'
+    } : {
+        textDecorationLine: 'none'
+    }
+
+    return (
+        <TouchableOpacity
+            onPress={() => props.completeFunction()}
+            style={{paddingVertical: 8, flexDirection: 'row', justifyContent: 'space-between'}}>
+            <Text style={[{fontSize: 18}, style]}>{props.item.text}</Text>
+
+            <TouchableOpacity
+                style={{padding: 8, backgroundColor: '#212121', justifyContent: 'center', alignItems: 'center', borderRadius: 8}}
+                onPress={() => props.deleteFunction()}>
+
+                <Text style={{color: '#fafafa'}}>X</Text>
+
+            </TouchableOpacity>
+
+        </TouchableOpacity>
+    );
+}
